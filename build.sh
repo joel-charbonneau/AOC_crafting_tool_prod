efc1e5ca17c418446e1c741519cfcf997e8ae831
joel-charbonneau <joel.charbonneau@gmail.com>
2024-12-09 09:42:20 -0500

    Fix requirements.txt and build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Install Git LFS
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs -y
-
-# Initialize Git LFS
-git lfs install
-git lfs pull
-
 # Install Python dependencies
 pip install -r requirements.txt
 
